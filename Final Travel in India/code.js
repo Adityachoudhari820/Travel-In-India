20b5705ba0e5c3100f3b2728a9e799151622c884
Aditya choudhari <75787426+Adityachoudhari820@users.noreply.github.com>
2021-09-13 18:36:20 +0530

    Add files via upload

---
+++ Final Travel in India/code.js
@@ -0,0 +1,104 @@
+onEvent("button1", "click", function( ) {
+  setScreen("Destination");
+  playSound("assets/category_human/character_jimmy_greetings_friend_1.mp3");
+});
+onEvent("button26", "click", function( ) {
+  playSound("assets/category_human/character_jimmy_really_2.mp3");
+  setScreen("Welcome");
+});
+onEvent("button2", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("The_Taj_Mahal");
+});
+onEvent("button14", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button3", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Fort_Aguada");
+});
+onEvent("button15", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button5", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Neil_Island");
+});
+onEvent("button16", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button4", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("The_Ridge");
+});
+onEvent("button17", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button6", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Dolphin's_Nose");
+});
+onEvent("button18", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button7", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Wagah_Border");
+});
+onEvent("button19", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button8", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Mattupetty_dam");
+});
+onEvent("button20", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button9", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Dal_lake");
+});
+onEvent("button21", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button10", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Chamera_Lake");
+});
+onEvent("button22", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button11", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Top_Station");
+});
+onEvent("button23", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button12", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Aina_Mahal_(Mirror_hall)");
+});
+onEvent("button24", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
+onEvent("button13", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Shivanasamudra_Falls");
+});
+onEvent("button25", "click", function( ) {
+  playSound("assets/default.mp3");
+  setScreen("Destination");
+});
